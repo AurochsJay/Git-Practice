5fad1cecfb1438a2d723abcb36e510f7f779f930
AurochsJay <rhim2301@gmail.com>
2023-10-25 20:21:05 +0900

    [fix] script

--- Git Unity Test/Assets/3.Script/kangminscript.cs
+++ Git Unity Test/Assets/3.Script/kangminscript.cs
@@ -5,27 +5,9 @@ using UnityEngine;
 public class kangminscript : MonoBehaviour
 {
 
-    [SerializeField] private Rigidbody rigidBody;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidBody = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 direction = new Vector3(1f, 2f, 3f);
-        rigidBody.AddForce(direction);
-        rigidBody.velocity *= 2f * Time.deltaTime;
-    }
-
-    //�̰��� �����̰� ������� ��ũ��Ʈ
-    //���� �������� �ʾҰ���
-    //�˼� ���� ��ũ��Ʈ�� ���������߰ڴ�.
-    //������ �����ֱ���
-    //������
+    
+    // 강민이 스크립트
+    // 바꾸는중
+    // 터트려야지
 
 }
